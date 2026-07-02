bd272e9cedfb34fb87c081e0d53459cb86f9b5ce
getinos <sujaydas0325@gmail.com>
2026-07-03 00:53:28 +0530

    fixes final

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+        mono: ["JetBrains Mono", "monospace"]
+      }
+    }
+  }
+}
